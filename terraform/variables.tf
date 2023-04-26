88cbc46c7a569c8e4bdd3f11c7a3d5a5115dd7ad
SIGHUP <sighup@SIGHUPs-MacBook-Pro.local>
2023-04-26 20:49:44 +0200

    Added basic terraform configuration for proxmox provisioning

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "pm_api_url" {
+    type = string
+    description = "The URL of the Proxmox API"
+}
+
+variable "pm_api_token_id" {
+    type = string
+    description = "The ID of the Proxmox API token"
+}
+
+variable "pm_api_token_secret"{
+    type = string
+    description = "The secret of the Proxmox API token"
+}
