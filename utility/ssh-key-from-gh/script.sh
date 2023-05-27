e8662b3edfa31c908b76476a47dca3e15c204424
Ettore Ciarcia <ettore.ciarcia@sighup.io>
2023-05-27 10:51:28 +0200

    Added bash version of ssh-from-gh

--- utility/ssh-key-from-gh/script.sh
+++ utility/ssh-key-from-gh/script.sh
@@ -3,29 +3,27 @@
 # Check if Go is installed
 if ! command -v go &> /dev/null; then
     echo "Go is not installed. Installing Go..."
-    
+
     # Download the Go binary archive
     wget https://golang.org/dl/go1.17.5.linux-amd64.tar.gz
-    
+
     # Extract the archive
-    tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
-    
+    sudo tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
+
     # Add Go binaries to the PATH environment variable
-    export PATH=$PATH:/usr/local/go/bin
-    
-    # Add Go environment variables to the profile file
-    echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
-    
-    # Reload the profile file
-    source ~/.profile
-    
+    echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee -a /etc/profile.d/go.sh
+
+    # Reload the profile
+    source /etc/profile.d/go.sh
+
     echo "Go has been installed."
 fi
 
+
 # Clone the repository
 echo "Cloning the repository..."
-git clone https://github.com/github/script.git
-cd script || exit 1
+git clone https://github.com/ettoreciarcia/homelab2.0.git
+cd homelab2.0/utility/ssh-key-from-gh/app || exit 1
 
 # Build and run the Go program
 echo "Building and running the program..."
@@ -33,7 +31,7 @@ go run main.go
 
 # Cleanup: Remove the cloned repository
 echo "Cleaning up..."
-cd ..
-rm -rf script
+cd ../..
+rm -rf homelab2.0
 
 echo "Script completed successfully."
